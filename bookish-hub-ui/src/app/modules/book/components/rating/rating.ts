09e188116069f42883ab3c99f9ffeaacc04cf1cc
Navila48 <tasnuvamorshed@gmail.com>
2025-09-21 21:35:57 +0600

    Implemented rating component

---
+++ bookish-hub-ui/src/app/modules/book/components/rating/rating.ts
@@ -0,0 +1,30 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  imports: [],
+  templateUrl: './rating.html',
+  styleUrl: './rating.scss'
+})
+export class Rating {
+
+  @Input() rating : number = 0;
+  maxRating = 5;
+
+  get fullStars() : number{
+    return Math.floor(this.rating);
+  }
+
+  get hasHalfStar() : boolean{
+    return this.rating % 1 !== 0;
+  }
+
+  get emptyStar() : number{
+    return this.maxRating - Math.ceil(this.rating);
+  }
+
+  range(n:number) : number[]{
+    return Array.from({length: n});
+  }
+
+}
